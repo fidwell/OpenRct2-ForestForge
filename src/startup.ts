f46b42db4bc8fedb0388bcc3ff7443288063fa56
Andrew <5436387+fidwell@users.noreply.github.com>
2024-09-19 22:43:08 -0400

    Added editor tab

--- src/startup.ts
+++ src/startup.ts
@@ -1,4 +1,4 @@
-import { button, dropdown, store, window } from "openrct2-flexui";
+import { button, dropdown, listview, store, tab, tabwindow } from "openrct2-flexui";
 import { BiomeFactory } from "./biomeFactory";
 import { SelectionTool } from "./selectionTool";
 
@@ -10,44 +10,75 @@ let isWindowOpen = false;
 let isActivated: boolean = false;
 const buttonText = store<string>("Activate tool");
 
-const pluginWindow = window({
+const tabToolActivator = [
+  dropdown({
+    items: biomes.map(b => b.name),
+    onChange: (index: number) => {
+      selectedBiome = index;
+    }
+  }),
+  button({
+    text: buttonText,
+    height: "28px",
+    onClick: () => {
+      if (isActivated) {
+        activeTool.cancel();
+      } else {
+        activeTool = new SelectionTool("forestForge", "tree_down");
+        activeTool.onCancel = cancel;
+        activeTool.activate();
+        buttonText.set("Cancel");
+        isActivated = true;
+      }
+    }
+  }),
+  button({
+    text: "Apply",
+    height: "28px",
+    onClick: () => {
+      activeTool?.apply(biomes[selectedBiome]);
+      buttonText.set("Activate tool");
+      isActivated = false;
+    }
+  })
+];
+
+const tabBiomeList = [
+  listview({
+    columns: ["Name", "Type"],
+    items: biomes.map(b => [
+      b.name,
+      "Built-in"
+    ]),
+    canSelect: true,
+    onClick: (item, number) => console.log(`row: ${item}, column: ${number}`)
+  })
+];
+
+const pluginWindow = tabwindow({
   title: "Forest Forge",
-  width: 200,
-  height: 105,
-  content: [
-    dropdown({
-      items: biomes.map(b => b.name),
-      onChange: (index: number) => {
-        selectedBiome = index;
-      }
+  width: 0,
+  height: 0,
+  tabs: [
+    tab({
+      image: "scenery_trees",
+      width: 200,
+      height: 140,
+      content: tabToolActivator
     }),
-    button({
-      text: buttonText,
-      height: "28px",
-      onClick: () => {
-        if (isActivated) {
-          activeTool.cancel();
-        } else {
-          activeTool = new SelectionTool("forestForge", "tree_down");
-          activeTool.onCancel = cancel;
-          activeTool.activate();
-          buttonText.set("Cancel");
-          isActivated = true;
-        }
-      }
-    }),
-    button({
-      text: "Apply",
-      height: "28px",
-      onClick: () => {
-        activeTool?.apply(biomes[selectedBiome]);
-        buttonText.set("Activate tool");
-        isActivated = false;
-      }
+    tab({
+      image: {
+        frameBase: 5277,
+        frameCount: 7,
+        frameDuration: 4,
+      } as ImageAnimation,
+      width: 250,
+      height: 170,
+      content: tabBiomeList
     })
   ],
   onClose: () => {
-    activeTool.cancel();
+    activeTool?.cancel();
     isWindowOpen = false;
   }
 });
