8e064347b97d5a015d46ef249b68da866d544e3d
Andrew <5436387+fidwell@users.noreply.github.com>
2024-10-02 18:38:32 -0400

    Use bit shifting for map coordinate conversion

--- src/selectionFiller.ts
+++ src/selectionFiller.ts
@@ -43,8 +43,8 @@ export abstract class SelectionFiller {
     sceneryDesc: SceneryDesc,
     quadrant: number) {
     const args = <SmallSceneryPlaceArgs>{
-      x: location.x * 32,
-      y: location.y * 32,
+      x: location.x << 5,
+      y: location.y << 5,
       z:  8 * (surfaceHeight - (sceneryDesc.verticalOffset ?? 0)),
       direction: context.getRandom(0, 4),
       object: sceneryDesc.object?.index,
